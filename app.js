9f9bfde3b1dd82f0c0dc6af171625780baafa28c
Gabriel Brandão <gabriel.silva.brandao7@gmail.com>
2019-08-14 22:28:25 -0300

    Add error handling

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const coursesRoutes = require('./api/routes/courses');
 
+app.use(morgan('dev'));
+
 app.use('/courses', coursesRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})  
 
 module.exports = app;
